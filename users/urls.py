641cdbb033948ceb74d128b1ffe83ec4d8b77cb6
Moshiur1143 <23101143@uap-bd.edu>
2026-04-04 21:58:49 +0600

    SCRUM-5: user registration added

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import register, user_login, dashboard
 
 urlpatterns = [
-    path('', home),
+    path('', dashboard, name='home'),
+    path('login/', user_login, name='login'),
+    path('register/', register, name='register'),
 ]
